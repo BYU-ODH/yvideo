d3fbe8c97aa5078a712ce650e06077ef471e36be
Santiago Verdu <santiago.verdu.01@gmail.com>
2017-05-16 04:27:59 -0600

    echo result

--- sbt_test_result.sh
+++ sbt_test_result.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 sudo docker wait runayamel_yvideo_1
-exit $(sudo docker inspect runayamel_yvideo_1 -f "{{ .State.ExitCode }}")
+result=$(sudo docker inspect runayamel_yvideo_1 -f "{{ .State.ExitCode }}")
+echo "$result"
+exit "$result"
 
