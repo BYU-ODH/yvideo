c112f067286a086c0318914ca908e7f94b1c62d7
Santiago Verdu <santiago.verdu.01@gmail.com>
2017-05-16 16:00:56 -0600

    half written test

--- test/EditableDocumentPermissionCheckerSpec.scala
+++ test/EditableDocumentPermissionCheckerSpec.scala
@@ -1,40 +1,52 @@
 import models.{Content, User, Course}
 import org.specs2.mutable._
+import org.specs2.execute._
+import play.api.test._
+import play.api.mvc
 
 import play.api.libs.json.Json
 import service.DocumentPermissionChecker
 
 /**
+ * The trait used to test the user controller
+ */
+trait UserController {
+  this: Controller =>
+
+  def index() = Action {
+    Ok("ok")
+  }
+}
+object UserController extends Controller with UserController
+
+/**
  * Tests all the methods in the course model
  */
-class EditableDocumentPermissionCheckerSpec extends Specification {
+class EditableDocumentPermissionCheckerSpec extends Specification with Results {
 
-  val personalResource = Json.obj(
-    "id" -> "josh1",
-    "attributes" -> Json.obj(
-      "ayamel_ownerType" -> "user",
-      "ayamel_ownerId" -> "5"
-    )
-  )
+  abstract class TestApplication extends WithApplication {
+	override def around[T: AsResult](t: => T): Result = super.around {
+	  setupData()
+	  t
+	}
 
-  val courseResource = Json.obj(
-    "id" -> "josh2",
-    "attributes" -> Json.obj(
-      "ayamel_ownerType" -> "course",
-      "ayamel_ownerId" -> "8"
-    )
-  )
+	def setupData() {
+	  // setup data
+	}
+  }
 
-  val globalResource = Json.obj(
-    "id" -> "josh3"
-  )
+  class Users() extends Controller with UserController
 
-  val course = Course(Some(8), "", "", "")
-  course.cache.teachers = Some(Nil)
+  "Users" should {
 
-  "The editable document permission checker" should {
-
-    true shouldEqual true
+	"be able to change their password" in new TestApplication {
+      val user = User(None, "", 'password, "fakeuser", None)
+      val usercontroller = new UserController()
+      val result: Future[Result] = usercontroller.changePassword.apply(FakeRequest())
+      val bodyText: String = contentAsString(result)
+      println(bodyText)
+      bodyText must be equalTo "ok"
+    }
 
   }
 }
