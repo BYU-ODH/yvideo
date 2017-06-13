d55a90bfa5289ef3dde642ee20e53b464064dbe6
Santiago Verdu <santiago.verdu.01@gmail.com>
2017-06-13 10:33:05 -0600

    wip

--- test/UserControllerSpec.scala
+++ test/UserControllerSpec.scala
@@ -1,30 +1,29 @@
-import models.{Content, User, Course}
-import controllers.Users
-
 import play.api.test._
 import play.api.mvc._
-import org.specs2.mock._
+import org.specs2.concurrent.ExecutionEnv
 import org.specs2.mutable._
 import play.api.test._
 import play.api.test.Helpers._
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
-object UserControllerSpec extends Specification with Mockito {
+import models.{Content, User, Course}
+import controllers.Users
+
+object UserControllerSpec extends Specification {
 
   class UsersTestController() extends Controller with Users
 
   "The User Controller" should {
 
-	"return account settings" in {
+    "return account settings" in { implicit ee: ExecutionEnv =>
       running(FakeApplication()) {
 
         implicit val user = User(None, "", 'password, "fakeuser", None)
         val controller = new UsersTestController()
         val result = controller.accountSettings(FakeRequest())
-        println(result.header.toString)
 
-        result.header.status shouldEqual 200
-
+        result map ( res => res.header.status shouldEqual 200) await
       }
     }
 
