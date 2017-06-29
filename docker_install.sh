778be1dc2fcb385b6445e5dc01dfea1820159e23
Santiago Verdu <santiago.verdu.01@gmail.com>
2017-06-29 16:35:39 -0600

    update docker version

--- docker_install.sh
+++ docker_install.sh
@@ -22,6 +22,6 @@ sudo apt -y install docker-engine
 
 # DOCKER COMPOSE
 sudo rm -f /usr/local/bin/docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.11.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.13.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
