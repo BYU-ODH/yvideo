84376250713ef1c40fa3f6aad3ae1bd8c2591608
Santiago Verdu <santiago.verdu.01@gmail.com>
2018-09-10 14:43:55 -0600

    Travis config / Encrypted conf file / sbt_test_result script changes

--- sbt_test_result.sh
+++ sbt_test_result.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "Waiting for runayamel_yvideo_1 docker container to exit"
-sudo docker wait runayamel_yvideo_test_1
-result=$(sudo docker inspect runayamel_yvideo_test_1 -f "{{ .State.ExitCode }}")
-echo "$result"
+sudo docker wait yvideo_yvideo_test_1
+result=$(sudo docker inspect yvideo_yvideo_test_1 -f "{{ .State.ExitCode }}")
+echo "ExitCode: [$result]"
 exit "$result"
 
