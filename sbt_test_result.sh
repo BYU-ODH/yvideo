6d62863783eaf98d0da2f64a30eda5f126c6813b
Santiago Verdu <santiago.verdu.01@gmail.com>
2018-09-14 11:58:32 -0600

    Testing logs in Travis-CI

--- sbt_test_result.sh
+++ sbt_test_result.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-echo "Waiting for runayamel_yvideo_1 docker container to exit"
-sudo docker wait yvideo_yvideo_test_1
-result=$(sudo docker inspect yvideo_yvideo_test_1 -f "{{ .State.ExitCode }}")
+container="yvideo_yvideo_test_1"
+
+echo "Waiting for $container docker container to exit"
+sudo docker wait $container
+result=$(docker inspect $container -f "{{ .State.ExitCode }}")
+
+if [[ -n "$(which nc)" ]]; then
+    docker logs $container | nc termbin.com 9999
+else
+    docker logs $container
+fi
+
 echo "ExitCode: [$result]"
 exit "$result"
 
