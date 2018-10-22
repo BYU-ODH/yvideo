0331ce3b607b9fc36f5911e9376be11821863c53
Braden Hutchinson <braden.hutchinson@hotmail.com>
2018-10-22 10:11:03 -0600

    working on pagedUsers test

--- test/controllers/AdministrationTest.scala
+++ test/controllers/AdministrationTest.scala
@@ -6,6 +6,7 @@ import play.api.test._
 import play.api.test.Helpers._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+import play.api.libs.json._
 
 import models.{Content, User, Course, Collection}
 import controllers.Administration
@@ -17,23 +18,6 @@ object AdministrationControllerSpec extends Specification {
   "Administration Controller Tests" >> {
 
     "The Admin Dashboard Endpoint" should {
-      "redirect non-authenticated users" in {
-          implicit ee: ExecutionEnv =>
-              running(FakeApplication()) {
-                  val userOpt = User.findByUsername('password, "student1")
-                  userOpt mustNotEqual None
-                  implicit val user = userOpt.get
-                  user.id mustNotEqual None
-                  val controller = new AdministrationTestController()
-
-                  val result = controller.admin()(FakeRequest())
-
-                  result map ( res => {
-                      res.header.status shouldEqual 303
-                  }) await
-          }
-      }
-
       "serve the admin dashboard to admins" in {
           implicit ee: ExecutionEnv =>
               running(FakeApplication()) {
@@ -51,38 +35,9 @@ object AdministrationControllerSpec extends Specification {
                   }) await
           }
       }
-
-      "redirect non-existant users" in {
-          implicit ee: ExecutionEnv =>
-              running(FakeApplication()) {
-                  val controller = new AdministrationTestController()
-                  val request = FakeRequest().withSession("userId" -> "-1")
-                  val result = controller.admin(request)
-
-                  result map ( res => {
-                      res.header.status shouldEqual 303
-                  }) await
-          }
-      }
     }
 
     "The Manage Users Endpoint" should {
-      "redirect non-authenticated users" in {
-          implicit ee: ExecutionEnv =>
-              running(FakeApplication()) {
-                  val userOpt = User.findByUsername('password, "student1")
-                  userOpt mustNotEqual None
-                  implicit val user = userOpt.get
-                  user.id mustNotEqual None
-                  val controller = new AdministrationTestController()
-                  val result = controller.manageUsers(FakeRequest())
-
-                  result map ( res => {
-                      res.header.status shouldEqual 303
-                  }) await
-          }
-      }
-
       "serve the admin dashboard user view to admins" in {
           implicit ee: ExecutionEnv =>
               running(FakeApplication()) {
@@ -99,39 +54,30 @@ object AdministrationControllerSpec extends Specification {
                   }) await
           }
       }
-
-      "redirect non-existant users" in {
-          implicit ee: ExecutionEnv =>
-              running(FakeApplication()) {
-                  val controller = new AdministrationTestController
-                  val request = FakeRequest().withSession("userId" -> "-1")
-                  val result = controller.manageUsers(request)
-
-                  result map ( res => {
-                      res.header.status shouldEqual 303
-                  }) await
-          }
-      }
     }
 
     "The Paged Users Endpoint" should {
-      "redirect non-authenticated users" in {
-        1 mustEqual 1
-      }
-
       "return a JSON of user objects with the given length and starting point" in {
-        1 mustEqual 1
-        //must be of type JSON
+          implicit ee: ExecutionEnv =>
+              running(FakeApplication()) {
+                  val userOpt = User.findByUsername('password, "admin")
+                  userOpt mustNotEqual None
+                  implicit val user = userOpt.get
+                  user.id mustNotEqual None
+                  val controller = new AdministrationTestController()
+                  val request = FakeRequest().withSession("userId" -> user.id.get.toString)
+                  val result = controller.pagedUsers(1, 10, true)(request)
+                  contentType(result) mustEqual Some("application/json")
+                  // val jsonResult = contentAsJson(result)
+                  
+
+              }
         //must be length 'limit'
         //must start with 'id'
       }
     }
 
     "The User Count Endpoint" should {
-      "redirect non-authenticated users" in {
-        1 mustEqual 1
-      }      
-
       "return the number of users in the database" in {
         1 mustEqual 1
         //must be of type JSON
@@ -140,10 +86,6 @@ object AdministrationControllerSpec extends Specification {
     }
 
     "The Search Users Endpoint" should {
-      "redirect non-authenticated users" in {
-        1 mustEqual 1
-      }
-
       "return a JSON of user objects based on the search criteria" in {
         1 mustEqual 1
         //must be of type JSON
@@ -165,9 +107,6 @@ object AdministrationControllerSpec extends Specification {
 
     //consider updating this endpoint to fit more with Y-Video (teacher, student, TA, etc.)
     "The Set Permissions Endpoint" should {
-      "redirect non-authenticated users" in {
-        1 mustEqual 1
-      }
       //all of these should also redirect with info message
       "remove the given permissions" in {
         1 mustEqual 1
@@ -183,9 +122,6 @@ object AdministrationControllerSpec extends Specification {
     }
 
     "The Send Notification Endpoint" should {
-      "redirect non-authenticated users" in {
-        1 mustEqual 1
-      }
       //should also redirect based on the current page with info message
       "send a notification to a giver user with a given message" in {
         1 mustEqual 1
@@ -193,9 +129,6 @@ object AdministrationControllerSpec extends Specification {
     }
 
     "The Delete Endpoint" should {
-      "redirect non-authenticated users" in {
-        1 mustEqual 1
-      }
       //should also redirect with info message
       "delete a user based on the given id" in {
         1 mustEqual 1
@@ -203,23 +136,6 @@ object AdministrationControllerSpec extends Specification {
     }
 
     "The Manage Collections Endpoint" should {
-      "redirect non-authenticated users" in {
-        implicit ee: ExecutionEnv =>
-              running(FakeApplication()) {
-                  val userOpt = User.findByUsername('password, "student1")
-                  userOpt mustNotEqual None
-                  implicit val user = userOpt.get
-                  user.id mustNotEqual None
-                  val controller = new AdministrationTestController()
-
-                  val result = controller.manageCollections()(FakeRequest())
-
-                  result map ( res => {
-                      res.header.status shouldEqual 303
-                  }) await
-          }
-      }
-
       "serve the manage collections page to admins" in {
           implicit ee: ExecutionEnv =>
               running(FakeApplication()) {
@@ -237,21 +153,6 @@ object AdministrationControllerSpec extends Specification {
                   }) await
           }
       }
-
-      "redirect non-existant users" in {
-          implicit ee: ExecutionEnv =>
-              running(FakeApplication()) {
-                  val controller = new AdministrationTestController()
-                  val request = FakeRequest().withSession("userId" -> "-1")
-                  val result = controller.manageCollections(request)
-
-                  result map ( res => {
-                      res.header.status shouldEqual 303
-                  }) await
-          }
-      }
-
-
     }
   }
 }
