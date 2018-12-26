3cf99917ed1cda4574d51f76c028b5c0762b9799
Santiago Verdu <santiago.verdu.01@gmail.com>
2018-12-26 17:05:11 -0600

    Travis autodeploy

--- sbt_test_result.sh
+++ sbt_test_result.sh
@@ -6,11 +6,12 @@ echo "Waiting for $container docker container to exit"
 sudo docker wait $container
 result=$(docker inspect $container -f "{{ .State.ExitCode }}")
 
-if [[ -n "$(which nc)" ]]; then
-    docker logs $container | nc termbin.com 9999
-else
-    docker logs $container
+if [[ -z "$(which nc)" ]]; then
+    apt-get install netcat -y
 fi
+# upload logs to termbin
+# logs seem to be truncated on travis-ci
+echo "SBT TEST LOGS: $(docker logs $container | nc termbin.com 9999)"
 
 echo "ExitCode: [$result]"
 exit "$result"
