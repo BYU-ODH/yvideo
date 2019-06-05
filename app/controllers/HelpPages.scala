a217aa3194bf87ae942eb27a3ef0698472826dc5
bradenhutch <braden.hutchinson@hotmail.com>
2019-06-04 20:45:49 -0600

    Removed help page views

--- app/controllers/HelpPages.scala
+++ app/controllers/HelpPages.scala
@@ -6,6 +6,7 @@ import play.api.mvc.{Action, Controller}
 import controllers.authentication.Authentication
 import models.{HelpPage, User}
 import dataAccess.ResourceController
+import play.api.libs.json._
 
 /**
  * Controller dealing with help pages
@@ -14,53 +15,15 @@ trait HelpPages {
   this: Controller =>
 
   /**
-   * Table of contents view
-   */
-  def tableOfContents = Action {
-    implicit request =>
-      implicit val user = request.session.get("userId").flatMap(id => User.findById(id.toLong))
-      val pages = HelpPage.list.groupBy(_.category)
-      Ok(views.html.help.toc(pages, ResourceController.baseUrl))
-  }
-
-  /**
-   * View a particular help page
-   * @param id The ID of the help page
-   */
-  def view(id: Long) = Action {
-    implicit request =>
-      implicit val user = request.session.get("userId").flatMap(id => User.findById(id.toLong))
-      HelpPage.findById(id).map( helpPage =>
-        Ok(views.html.help.view(helpPage, ResourceController.baseUrl))
-      ).getOrElse(Errors.notFound)
-  }
-
-  /**
-   * Edit a particular help page
-   * @param id The ID of the help page
-   */
-  def edit(id: Long) = Authentication.authenticatedAction() {
-    implicit request =>
-      implicit user =>
-        Authentication.enforcePermission("admin") {
-          val helpPage = HelpPage.findById(id)
-          Future(Ok(views.html.help.edit(helpPage)))
-        }
-  }
-
-  /**
    * Delete a particular help page
    * @param id The ID of the help page
    */
-  def delete(id: Long) = Authentication.authenticatedAction() {
+  def delete(id: Long) = Authentication.secureAPIAction() {
     implicit request =>
       implicit user =>
-        Authentication.enforcePermission("admin") {
+        Authentication.enforcePermissionAPI("admin") {
           HelpPage.findById(id).map(_.delete())
-          Future {
-            Redirect(routes.HelpPages.tableOfContents())
-              .flashing("info" -> "Help page deleted.")
-          }
+              Ok(Json.obj("message" -> "Help page deleted."))
         }
   }
 
@@ -68,27 +31,23 @@ trait HelpPages {
    * Save/update a particular help page
    * @param id The ID of the help page
    */
-  def save(id: Long) = Authentication.authenticatedAction(parse.urlFormEncoded) {
+  def save(id: Long) = Authentication.secureAPIAction(parse.urlFormEncoded) {
     implicit request =>
       implicit user =>
-        Authentication.enforcePermission("admin") {
+        Authentication.enforcePermissionAPI("admin") {
 
           val helpPage = HelpPage.findById(id)
           val title = request.body("title")(0)
           val contents = request.body("contents")(0)
           val category = request.body("category")(0)
 
-          Future {
-            if (helpPage.isDefined) {
-              helpPage.get.copy(title = title, contents = contents, category = category).save
-              Redirect(routes.HelpPages.view(helpPage.get.id.get))
-                .flashing("info" -> "Help page saved.")
-            } else {
-              // Create new
-              val newHelpPage = HelpPage(None, title, contents, category).save
-              Redirect(routes.HelpPages.view(newHelpPage.id.get))
-                .flashing("info" -> "Help page created.")
-            }
+          if (helpPage.isDefined) {
+            helpPage.get.copy(title = title, contents = contents, category = category).save
+            Ok(Json.obj("message" -> "Help page saved."))
+          } else {
+            // Create new
+            val newHelpPage = HelpPage(None, title, contents, category).save
+            Ok(Json.obj("info" -> "Help page created."))
           }
         }
   }
